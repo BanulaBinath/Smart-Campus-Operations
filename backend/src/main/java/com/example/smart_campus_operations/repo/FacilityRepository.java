00e1e6539afd81dccadd504b25fc1f644cd8878a
BanulaBinath <banulabinath.edu@gmail.com>
2026-04-17 11:46:39 +0530

    Add login/signup UI

--- backend/src/main/java/com/example/smart_campus_operations/repo/FacilityRepository.java
+++ backend/src/main/java/com/example/smart_campus_operations/repo/FacilityRepository.java
@@ -1,5 +1,15 @@
 package com.example.smart_campus_operations.repo;
 
-public interface FacilityRepository {
+import com.example.smart_campus_operations.model.Facility;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface FacilityRepository extends JpaRepository<Facility, Long> {
+
+    List<Facility> findByType(String type);
+
+    List<Facility> findByStatus(String status);
 }

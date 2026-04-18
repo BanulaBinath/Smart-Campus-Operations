ca81dac615fdb4c9ed4c2fe328e0ad9b55830727
Al Ameen Aaryan <alameenaaryan@gmail.com>
2026-04-18 19:04:22 +0530

    feat: add create ticket form with attachment limit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './Components/home/login';
 import SignUp from './Components/home/signUp';
 import UserDashboard from './Components/ticket/UserDashboard';
+import CreateTicket from './Components/ticket/CreateTicket';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/tickets" element={<UserDashboard />} />
+      <Route path="/tickets/create" element={<CreateTicket />} />
     </Routes>
   );
 }

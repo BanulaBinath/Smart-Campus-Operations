53875397d332bc470175f4494bd6011f51a9104e
PrathieshS <153347825+PrathieshS@users.noreply.github.com>
2026-04-18 10:59:37 +0530

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './Components/home/login';
 import SignUp from './Components/home/signUp';
 import MyBookingsPage from './Components/MyBookingsPage';
-import AdminBookingsPage from './Components/AdminBookingsPage';
+import AdminPage from './Components/adminPage';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/bookings" element={<MyBookingsPage />} />
-      <Route path="/admin/bookings" element={<AdminBookingsPage />} />
+      <Route path="/admin/*" element={<AdminPage />} />
     </Routes>
   );
 }

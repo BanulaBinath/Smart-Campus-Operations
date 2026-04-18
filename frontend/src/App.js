966e201ad227ecc53ac25b711f0c237ebd5ac6f2
PrathieshS <153347825+PrathieshS@users.noreply.github.com>
2026-04-18 10:47:08 +0530

    Fixed issues with GIT HUB repo and updated adminpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './Components/home/login';
 import SignUp from './Components/home/signUp';
+import MyBookingsPage from './Components/MyBookingsPage';
+import AdminBookingsPage from './Components/AdminBookingsPage';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/bookings" element={<MyBookingsPage />} />
+      <Route path="/admin/bookings" element={<AdminBookingsPage />} />
     </Routes>
   );
 }
